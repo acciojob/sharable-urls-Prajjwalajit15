96f31a75b3663e4201a6d67b8941b18538c0d2f7
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-12-14 13:35:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+ function updateUrl() {
+        var name = document.getElementById('name').value;
+        var year = document.getElementById('year').value;
+
+        // Construct the URL with query string
+        var url = 'https://localhost:8080/?name=' + encodeURIComponent(name) + '&year=' + encodeURIComponent(year);
+
+        // Update the h3 element with the generated URL
+        document.getElementById('url').textContent = url;
+    }
